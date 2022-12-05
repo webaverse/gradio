2e0f1167a5f11522a74db1bdea3debacfa36d925
Avaer Kazmer <a@modules.io>
2022-12-05 09:39:54 -0500

    Update networking.py

--- gradio/networking.py
+++ gradio/networking.py
@@ -18,6 +18,7 @@ import uvicorn
 from gradio.routes import App
 from gradio.tunneling import create_tunnel
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Response
 
 if TYPE_CHECKING:  # Only import for type checking (to avoid circular imports).
     from gradio.blocks import Blocks
@@ -161,6 +162,20 @@ def start_server(
     async def add_cors_headers(request: Request, call_next):
         print("server middleware")
 
+        if (request.method == "OPTIONS"):
+            print("got options 1")
+            response = Response()
+            response.headers["Access-Control-Allow-Origin"] = "*"
+            response.headers["Access-Control-Allow-Headers"] = "*"
+            response.headers["Access-Control-Allow-Methods"] = "*"
+            response.headers["Access-Control-Expose-Headers"] = "*"
+            response.headers["Cross-Origin-Opener-Policy"] = "same-origin"
+            response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
+            response.headers["Cross-Origin-Resource-Policy"] = "cross-origin"
+            print("got options 2")
+            return response
+
+        print("got regular")
         response = await call_next(request)
         response.headers["Access-Control-Allow-Origin"] = "*"
         response.headers["Access-Control-Allow-Headers"] = "*"
